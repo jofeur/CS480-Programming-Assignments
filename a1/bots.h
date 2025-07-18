dee11a5b1d4e04c597280b49006806c949daf331
jofeur <jurrea7806@sdsu.edu>
2025-07-18 10:31:24 -0700

    Update bots.h

--- a1/bots.h
+++ a1/bots.h
@@ -1,3 +1,11 @@
+/*
+    Authors: Jose Urrea, Abdihakim Ahmed
+    Class Account Username: cssc1465, cssc1402
+    Class Informartion: CS 480, Summer 2025
+    Assignment Information : Programming Assignment #1
+    File : bots.h
+*/
+
 #ifndef BOTS_H
 #define BOTS_H
 
