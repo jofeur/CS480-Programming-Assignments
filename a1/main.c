94f0e170295b0a40ead7d717f5f95232122344da
jofeur <jurrea7806@sdsu.edu>
2025-07-17 22:21:16 -0700

    Update main.c

--- a1/main.c
+++ a1/main.c
@@ -1,1 +1,14 @@
+/*
+    Authors: Jose Urrea, ......
+    Class Account Username: cssc1465, .....
+    Class Informartion: CS 480, Summer 2025
+    Assignment Information : Programming Assignment #1
+    File : main.c
+*/
 
+#include "bots.h"
+
+int main() {
+    run_threads();
+    return 0;
+}
